827b4c6f59cfa44933acea63481c02661844baa1
Jerry Hamlet <jerry@hamletink.com>
2012-03-27 01:13:48 -0700

    Update test-sample.js

--- test/test-samples.js
+++ test/test-samples.js
@@ -3,86 +3,67 @@
 var should = require("should"),
     ExParser = require("../"),
     Expression = ExParser.Expression,
-    textSize = parseInt(process.env["TEXT_MULTIPLE"], 10),
+    textSize = parseInt(process.env.TEXT_MULTIPLE, 10) || 2000,
     FS = require("fs"),
-    WORD_PATTERN = /(([\w_-]+\.?)+)/,
-    INCLUDE_EXP = new Expression([/@/, /include/, /\s+/, /([^\s]+)/]),
-    DEPEND_EXP = new Expression([/@/, /depends?/, /\s+/, /([^\s]+)/])
-    REQUIRE_EXP = new Expression([/@/, /requires?/, /\s+/, /([^\s]+)/]),
+    WORD_PATTERN = /(([\w_\-]+\.?)+)/,
+    INCLUDE_EXP     = new Expression([/@/, /include/, /\s+/, /([^\s]+)/]),
+    DEPEND_EXP      = new Expression([/@/, /depends?/, /\s+/, /([^\s]+)/]),
+    REQUIRE_BC_EXP  = new Expression([/@/, /requires?/, /\s+/, /([^\s]+)/]),
+    REQUIRE_FN_EXP  = new Expression([/\brequire\(\"/, /([^\"]+)/, /"\)/]),
+    REQUIRE_EXP     = new Expression([[REQUIRE_BC_EXP, REQUIRE_FN_EXP]]),
     REPLACEMENT_EXP = new Expression([/\$/, WORD_PATTERN, /\$/])
 ;
 
+// console.log(REQUIRE_EXP.getStartPattern());
+
 suite("Samples", function () {
-    var text = FS.readFileSync(__dirname + "/sample-patterns.txt"),
-        includes = [],
-        depends  = [],
-        requires = [],
-        tokens   = [],
+    var text,
+        includes, depends, requires, tokens,
+        parser
+    ;
+
+    setup(function () {
+        text = FS.readFileSync(__dirname + "/sample-patterns.txt");
+        includes = [];
+        depends  = [];
+        requires = [];
+        tokens   = [];
         parser = new ExParser({
             expressions: {
-                include: INCLUDE_EXP,
-                depend: DEPEND_EXP,
-                require: REQUIRE_EXP,
+                include:     INCLUDE_EXP,
+                depend:      DEPEND_EXP,
+                require:     REQUIRE_EXP,
                 replacement: REPLACEMENT_EXP
             },
-            handlers: {
+            listeners: {
                 include: function (captures) {
                     var path = captures[1];
+                    // console.log("INCLUDE " + path);
                     includes.push(path);
                 },
                 depend: function (captures) {
                     var path = captures[1];
+                    // console.log("DEPEND " + path);
                     depends.push(path);
                 },
                 require: function (captures) {
                     var path = captures[1];
+                    // console.log("REQUIRE " + path);
                     requires.push(path);
                 },
                 replacement: function (captures) {
                     var token = captures[1];
+                    // console.log("TOKEN " + token);
                     tokens.push(token);
                 }
             }
-        }),
-        start, finish
-    ;
-    
-    text = Array(textSize || 2000).join(text);
-    console.log(text.length);
+        });
 
-    console.log(parser.getPivotPattern());
-    
-    start = Date.now();
-    parser.parse(text);
-    finish = Date.now();
-    
-    test("includes", function () {
-        // console.log("INCLUDES");
-        // includes.forEach(function (path) {
-        //     console.log(Array(4).join(" ") + path);
-        // });
-    });
-
-    test("depends", function () {
-        // console.log("DEPENDS");
-        // depends.forEach(function (path) {
-        //     console.log(Array(4).join(" ") + path);
-        // });
-    });
-
-    test("requires", function () {
-        // console.log("REQUIRES");
-        // requires.forEach(function (path) {
-        //     console.log(Array(4).join(" ") + path);
-        // });
-    });
-
-    test("replacements", function () {
-        // console.log("REPLACEMENTS");
-        // tokens.forEach(function (path) {
-        //     console.log(Array(4).join(" ") + path);
-        // });
+        text = Array(textSize).join(text);
     });
     
-    console.log((finish - start) + "ms");
+    test("Samples", function () {
+        parser.parse(text);
+    });
+
 });
